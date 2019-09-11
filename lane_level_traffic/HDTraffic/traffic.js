0c2aab5eb641902656ba948e1cc6d35d1050d7df
terencezhao <terencezhao@uchicago.edu>
2019-09-11 00:40:00 -0500

    Add files via upload

---
+++ lane_level_traffic/HDTraffic/traffic.js
@@ -0,0 +1,10 @@
+﻿[
+{"lane" : 0, "speed" : 20, "density" : 20, "flow" : 20},
+{"lane" : 1, "speed" : 20, "density" : 20, "flow" : 20},
+{"lane" : 2, "speed" : 20, "density" : 20, "flow" : 20},
+{"lane" : 3, "speed" : 20, "density" : 20, "flow" : 20},
+{"lane" : 4, "speed" : 20, "density" : 20, "flow" : 20},
+{"lane" : 5, "speed" : 20, "density" : 20, "flow" : 20},
+{"lane" : 6, "speed" : 20, "density" : 20, "flow" : 20},
+{"lane" : 7, "speed" : 20, "density" : 20, "flow" : 20}
+]
